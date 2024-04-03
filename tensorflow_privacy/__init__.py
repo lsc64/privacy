596840c56764f1d9adeb4b7c9b54116f3766a1a9
Dariush Wahdany <dariush.wahdany@aisec.fraunhofer.de>
2024-04-03 23:33:22 +0200

    fix: remove v1 import

--- tensorflow_privacy/__init__.py
+++ tensorflow_privacy/__init__.py
@@ -15,77 +15,26 @@
 
 import sys
 
-from tensorflow_privacy.version import __version__  # pylint: disable=g-bad-import-order
-
 # pylint: disable=g-import-not-at-top
 
-if hasattr(sys, 'skip_tf_privacy_import'):  # Useful for standalone scripts.
-  pass
+if hasattr(sys, "skip_tf_privacy_import"):  # Useful for standalone scripts.
+    pass
 else:
-  # TensorFlow v1 imports
-  from tensorflow_privacy import v1
+    # TensorFlow v1 imports
+    # from tensorflow_privacy import v1
 
-  # Analysis
-  from tensorflow_privacy.privacy.analysis.compute_dp_sgd_privacy_lib import compute_dp_sgd_privacy
-  from tensorflow_privacy.privacy.analysis.compute_dp_sgd_privacy_lib import compute_dp_sgd_privacy_statement
-  from tensorflow_privacy.privacy.analysis.tree_aggregation_accountant import compute_rdp_tree_restart
-  from tensorflow_privacy.privacy.analysis.tree_aggregation_accountant import compute_rdp_single_tree
-  from tensorflow_privacy.privacy.analysis.tree_aggregation_accountant import compute_zcdp_single_tree
+    # Analysis
 
-  # DPQuery classes
-  from tensorflow_privacy.privacy.dp_query.dp_query import DPQuery
-  from tensorflow_privacy.privacy.dp_query.dp_query import SumAggregationDPQuery
-  from tensorflow_privacy.privacy.dp_query.discrete_gaussian_query import DiscreteGaussianSumQuery
-  from tensorflow_privacy.privacy.dp_query.distributed_discrete_gaussian_query import DistributedDiscreteGaussianSumQuery
-  from tensorflow_privacy.privacy.dp_query.distributed_skellam_query import DistributedSkellamSumQuery
-  from tensorflow_privacy.privacy.dp_query.gaussian_query import GaussianSumQuery
-  from tensorflow_privacy.privacy.dp_query.nested_query import NestedQuery
-  from tensorflow_privacy.privacy.dp_query.no_privacy_query import NoPrivacyAverageQuery
-  from tensorflow_privacy.privacy.dp_query.no_privacy_query import NoPrivacySumQuery
-  from tensorflow_privacy.privacy.dp_query.normalized_query import NormalizedQuery
-  from tensorflow_privacy.privacy.dp_query.quantile_estimator_query import QuantileEstimatorQuery
-  from tensorflow_privacy.privacy.dp_query.quantile_estimator_query import NoPrivacyQuantileEstimatorQuery
-  from tensorflow_privacy.privacy.dp_query.quantile_adaptive_clip_sum_query import QuantileAdaptiveClipSumQuery
-  from tensorflow_privacy.privacy.dp_query.quantile_adaptive_clip_tree_query import QAdaClipTreeResSumQuery
-  from tensorflow_privacy.privacy.dp_query import restart_query
-  from tensorflow_privacy.privacy.dp_query import tree_aggregation
-  from tensorflow_privacy.privacy.dp_query.restart_query import RestartQuery
-  from tensorflow_privacy.privacy.dp_query.tree_aggregation_query import TreeCumulativeSumQuery
-  from tensorflow_privacy.privacy.dp_query.tree_aggregation_query import TreeResidualSumQuery
-  from tensorflow_privacy.privacy.dp_query.tree_range_query import TreeRangeSumQuery
+    # DPQuery classes
 
-  # Estimators
-  from tensorflow_privacy.privacy.estimators.dnn import DNNClassifier
+    # Estimators
 
-  # Keras Models
-  from tensorflow_privacy.privacy.keras_models.dp_keras_model import DPModel
-  from tensorflow_privacy.privacy.keras_models.dp_keras_model import DPSequential
-  from tensorflow_privacy.privacy.keras_models.dp_keras_model import make_dp_model_class
+    # Keras Models
 
-  # Optimizers
-  from tensorflow_privacy.privacy.optimizers.dp_optimizer_keras import GenericDPAdagradOptimizer
-  from tensorflow_privacy.privacy.optimizers.dp_optimizer_keras import GenericDPAdamOptimizer
-  from tensorflow_privacy.privacy.optimizers.dp_optimizer_keras import GenericDPSGDOptimizer
-  from tensorflow_privacy.privacy.optimizers.dp_optimizer_keras import DPKerasAdagradOptimizer
-  from tensorflow_privacy.privacy.optimizers.dp_optimizer_keras import DPFTRLTreeAggregationOptimizer
-  from tensorflow_privacy.privacy.optimizers.dp_optimizer_keras import DPKerasAdamOptimizer
-  from tensorflow_privacy.privacy.optimizers.dp_optimizer_keras import DPKerasSGDOptimizer
-  from tensorflow_privacy.privacy.optimizers.dp_optimizer_keras import make_gaussian_query_optimizer_class
-  from tensorflow_privacy.privacy.optimizers.dp_optimizer_keras import make_keras_generic_optimizer_class
-  from tensorflow_privacy.privacy.optimizers.dp_optimizer_keras import make_keras_optimizer_class
+    # Optimizers
 
-  from tensorflow_privacy.privacy.optimizers.dp_optimizer_keras_vectorized import VectorizedDPKerasAdagradOptimizer
-  from tensorflow_privacy.privacy.optimizers.dp_optimizer_keras_vectorized import VectorizedDPKerasAdamOptimizer
-  from tensorflow_privacy.privacy.optimizers.dp_optimizer_keras_vectorized import VectorizedDPKerasSGDOptimizer
-  from tensorflow_privacy.privacy.optimizers.dp_optimizer_keras_vectorized import make_vectorized_keras_optimizer_class
+    # Logistic regression
 
-  # Logistic regression
-  from tensorflow_privacy.privacy.logistic_regression.datasets import linearly_separable_labeled_examples
-  from tensorflow_privacy.privacy.logistic_regression.datasets import synthetic_linearly_separable_data
+    pass
 
-  from tensorflow_privacy.privacy.logistic_regression.multinomial_logistic import logistic_objective_perturbation
-  from tensorflow_privacy.privacy.logistic_regression.multinomial_logistic import logistic_dpsgd
-
-  from tensorflow_privacy.privacy.logistic_regression.single_layer_softmax import single_layer_softmax_classifier
-
-  # module `bolt_on` not yet available in this version of TF Privacy
+    # module `bolt_on` not yet available in this version of TF Privacy
